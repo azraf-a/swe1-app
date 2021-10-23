0259f9f1fc8bcd07cb977b1c98627130aad31976
azraf-a <aa8542@nyu.edu>
2021-10-23 19:33:47 -0400

    Additional edits to resolve errors

--- polls/tests.py
+++ polls/tests.py
@@ -32,8 +32,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
-
-
+        
 
 # class QuestionIndexViewTests(TestCase):
 #     def test_no_questions(self):
